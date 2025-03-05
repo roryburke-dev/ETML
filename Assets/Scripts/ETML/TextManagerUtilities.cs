f25bed04e3e7a6116bc57bcf06b6a3938505dcf1
InternetJones <davis.indiana7@gmail.com>
2025-03-04 22:19:07 -0600

    Added Enums

--- Assets/Scripts/ETML/TextManagerUtilities.cs
+++ Assets/Scripts/ETML/TextManagerUtilities.cs
@@ -2,6 +2,33 @@ namespace ETML
 {
     public class TextManagerUtilities
     {
-    
+    public enum ENUMFONTS
+    {
+        CottonCloud,
+        BadMood,
+        Heartless
+    }
+    public enum ENUMMOODS
+    {
+        Angry,
+        Ecstatic,
+        Confused,
+        Depressed,
+        Scared,
+        Tired,
+        Relieved,
+        Bored
+    }
+    public enum ENUMCOLORS
+    {
+        Red,
+        Yellow,
+        Pink,
+        Blue,
+        Purple,
+        Grey,
+        BlueGrey,
+        Green
+    }
     }
 }
